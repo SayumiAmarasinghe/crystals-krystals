392608061602bc95cd5eeb32462e5787a709f153
SayumiAmarasinghe <148291424+SayumiAmarasinghe@users.noreply.github.com>
2026-06-16 17:37:32 -0700

    adjusted routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,11 @@ import Home from './Pages/Home';
 import Shop from './Pages/Shop';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
+import CustomOrder from './Pages/CustomOrder';
+import Earrings from './Pages/Earrings';
+import Necklace from './Pages/Necklace';
+import Rings from './Pages/Rings';
+import Miscellaneous from './Pages/Miscellaneous';
 
 function App() {
   return (
@@ -22,7 +27,11 @@ function App() {
           <Route path="/shop" element={<Shop />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          
+          <Route path="/custom-order" element={<CustomOrder />} />
+          <Route path="/shop/earrings" element={<Earrings />} />
+          <Route path="/shop/necklaces" element={<Necklace />} />
+          <Route path="/shop/rings" element={<Rings />} />
+          <Route path="shop/miscellaneous" element={<Miscellaneous />} />
           {/* Optional: A catch-all route for 404 Not Found */}
           <Route path="*" element={<div style={{textAlign: 'center', padding: '4rem'}}>Page not found!</div>} />
         </Routes>
---
+++ frontend/src/Pages/Miscellaneous.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+//import './About.css';
+const Miscellaneous = () => {
+  return (
+    <div className="miscellaneous-page">
+        
+    </div>
+  );
+};
+
+export default Miscellaneous;
